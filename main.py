a160228c8d164f93f1d3f59adab25a69a705a196
ankarandikar <karandikara02@gmail.com>
2022-04-25 15:48:06 -0400

    testing pygame

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
