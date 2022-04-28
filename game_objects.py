fa0330dc3380a2b1894fa789c7712d0f86f214eb
ankarandikar <karandikara02@gmail.com>
2022-04-27 20:34:29 -0400

    Get image generation working.

--- game_objects.py
+++ game_objects.py
@@ -1,21 +1,27 @@
 """
 
 """
+import pygame
 
-class GhibliGameObject:
-    def __init__(self):
+class GhibliGameObject(pygame.sprite.Sprite):
+    def create_object(self, img, x, y, scale, xflip, yflip):
         """
-        Initialize position, velocity, hitbox, graphics.
+        Initialize image path, position, hitbox.
         """
-        pass
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.image.load(img)
+        self.img = pygame.transform.scale(self.img, (int(self.img.get_width() * scale), int(self.img.get_height() * scale)))
+        self.img = pygame.transform.flip(self.img, xflip, yflip)
+        self.rect = self.img.get_rect()
+        self.rect.center = (x, y)
 
 class GhibliGameSprite(GhibliGameObject):
     def __init__(self):
-        pass
+        self.create_object('images/kiki.png',100,100,0.5,True,False)
 
 class GhibliGamePackage(GhibliGameObject):
-    def __init__(self):
-        pass
+    def __init__(self, y, scale):
+        self.create_object('images/kiki.png',700,y,scale,False,False)
 
 class GhibliGameObstacle(GhibliGameObject):
     def __init__(self):
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from game_objects import GhibliGameObject, GhibliGamePackage, GhibliGameSprite
 
 pygame.init()
 
@@ -6,11 +7,20 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+player = GhibliGameSprite()
+package = GhibliGamePackage(500,0.25)
+
 run = True
 
 while run:
+
+    screen.blit(player.img, player.rect)
+    screen.blit(package.img,package.rect)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
